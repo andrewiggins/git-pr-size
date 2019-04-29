47edff9f14e990615e0671e95a454248deb26c35
Andre Wiggins <andrewiggins@live.com>
2019-04-29 11:22:06 -0700

    Get commits from local git log command

---
+++ src/git.ts
@@ -0,0 +1,43 @@
+import { ExecOptions } from 'child_process';
+import { execAsync } from './cmd';
+
+export interface Commit {
+	/** The object id (i.e. hash or sha1) of the commit */
+	oid: string;
+
+	/** The subject of the commit */
+	subject: string;
+}
+
+const defaultArgs = "--first-parent";
+const logFormat = ' --pretty="format:%H %s" ';
+function getLogCommand(oldArgs = defaultArgs) {
+	const i = oldArgs.search(/(?:\W|^)--(?:\W|$)/);
+	const cmd = "git log ";
+
+	if (i == -1) {
+		return cmd + oldArgs + logFormat;
+	} else {
+		return cmd + oldArgs.slice(0, i) + logFormat + oldArgs.slice(i);
+	}
+}
+
+function parseLogOutput(stdout: string): Commit[] {
+	return stdout
+		.split("\n")
+		.map(line => line.trim())
+		.map(line => {
+			const i = line.indexOf(" ");
+			return {
+				oid: line.slice(0, i),
+				subject: line.slice(i + 1)
+			};
+		});
+}
+
+export async function getCommits(logArgs?: string, options?: ExecOptions): Promise<Commit[]> {
+	const cmd = getLogCommand(logArgs);
+	console.log(cmd);
+	const { stdout } = await execAsync(cmd, options);
+	return parseLogOutput(stdout.toString());
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import * as path from 'path';
-import { readFileSync } from 'fs';
-import { createFetch, fetchMasterPRs } from './github';
+import * as path from "path";
+import { readFileSync } from "fs";
+import { createFetch, fetchMasterPRs } from "./github";
+import { getCommits } from "./git";
 
-const access_token = readFileSync(path.join(__dirname, '.access_token'), 'utf8').trim();
+const access_token = readFileSync(
+	path.join(__dirname, ".access_token"),
+	"utf8"
+).trim();
 
 async function getPRs() {
 	const fetch = createFetch(access_token);
-	const prs = await fetchMasterPRs(fetch, 'developit/preact', 10);
+	const prs = await fetchMasterPRs(fetch, "developit/preact", 10);
 
 	console.log(prs);
 }
 
-getPRs();
+async function getCommitsTest() {
+	const commits = await getCommits('-n 6 -- ./src', { cwd: "D:/github/developit/preact" });
+	console.log(commits);
+}
+
+// getPRs();
+getCommitsTest();
